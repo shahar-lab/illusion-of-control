6e669d45509f76b63ffd33aff8767d809e47a6b2
alonkg <alonkagan@mail.tau.ac.il>
2026-07-14 22:47:47 +0300

    update sample amount

--- analysis/qval_and_decay_eval_param_recovery/code/fit_stan.R
+++ analysis/qval_and_decay_eval_param_recovery/code/fit_stan.R
@@ -21,8 +21,8 @@ fit <- model$sample(
   data            = stan_data,
   chains          = 4,
   parallel_chains = 4,
-  iter_warmup     = 500,
-  iter_sampling   = 500,
+  iter_warmup     = 2000,
+  iter_sampling   = 3000,
   refresh         = 200
 )
 
